fe69c9ab31934ed7fa98dfaf4b61c87169328cde
Shozen Dan <shozen.dan@gmail.com>
2018-11-30 11:21:50 +0900

    reformatted the API controller structure

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+var addController = require('./addController');
+var getController = require('./getController');
+var delController = require('./delController');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+module.exports = function(app){
+    addController(app);
+    getController(app);
+    delController(app);
+}
