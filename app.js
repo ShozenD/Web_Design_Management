e0d19cf85c8aa22f9569fff8ed3eb380bd621763
Shozen Dan <shozen.dan@gmail.com>
2018-11-30 10:06:58 +0900

    Now renders index.html to mainscreen

--- app.js
+++ app.js
@@ -22,26 +22,17 @@ mongoose.connect(config.getDbConnctionString(), {useNewUrlParser: true});
 setupController(app);
 apiController(app);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.sendFile('./views/index.html');
+});
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-app.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-//app.use('/', indexRouter);
-//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
