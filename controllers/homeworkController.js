0debfd2b9eed64e38e788c6d644467248e7ec2ac
Shozen Dan <shozen.dan@gmail.com>
2018-12-02 14:42:05 +0900

    Recreated database structure from scratch and created an API to initialize it

---
+++ controllers/homeworkController.js
@@ -0,0 +1,19 @@
+Homework = require('../models/homeworkModel');
+
+const HomeworkController = {
+    save: function(params, cb) {
+        // Give a homework
+        console.log("homeworkController give method called");
+
+        var homework = new Homework(params);
+
+        console.log("homework: ", homework);
+
+        homework.save(function (err, saved_homework){
+            if(err) return cb(err);
+            cb(null, saved_homework);
+        });
+    }
+}
+
+module.exports = HomeworkController;
